920d22ea3a3ec04728164547d25e95bfeb69b545
Imran <strongsoda2@gmail.com>
2018-05-27 18:02:27 +0530

    Bug Fix

--- sw.js
+++ sw.js
@@ -23,6 +23,46 @@ self.addEventListener('install', function(event) {
 
 
 // Updating the service worker.
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+  
+//           // IMPORTANT: Clone the request. A request is a stream and
+//           // can only be consumed once. Since we are consuming this
+//           // once by cache and once by the browser for fetch, we need
+//           // to clone the response.
+//           var fetchRequest = event.request.clone();
+  
+//           return fetch(fetchRequest).then(
+//             function(response) {
+//               // Check if we received a valid response
+//               if(!response || response.status !== 200 || response.type !== 'basic') {
+//                 return response;
+//               }
+  
+//               // IMPORTANT: Clone the response. A response is a stream
+//               // and because we want the browser to consume the response
+//               // as well as the cache consuming the response, we need
+//               // to clone it so we have two streams.
+//               var responseToCache = response.clone();
+  
+//               caches.open(CACHE_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request, responseToCache);
+//                 });
+  
+//               return response;
+//             }
+//           );
+//         })
+//       );
+//   });
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request)
@@ -31,36 +71,10 @@ self.addEventListener('fetch', function(event) {
           if (response) {
             return response;
           }
-  
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
-          var fetchRequest = event.request.clone();
-  
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-  
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-  
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            }
-          );
-        })
-      );
+          return fetch(event.request);
+        }
+      )
+    );
   });
 
 //  By default, fetching a resource from a third party URL will fail if it doesn't support CORS.
